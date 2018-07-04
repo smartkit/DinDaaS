e1cdeb175840e5a63fae52d71e0f8e69c75455bd
yangboz <youngwelle@gmail.com>
2018-07-04 10:33:03 +0800

    ADDED:react-app example code;

---
+++ DApps/eth-ipfs/src/index.js
@@ -0,0 +1,3 @@
+/*https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-a-stylesheet*/
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
